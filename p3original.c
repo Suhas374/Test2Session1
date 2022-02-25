aa98e59d067e781b86b614065f53e6fe5fdbacc3
suhas A <97019943+Suhas374@users.noreply.github.com>
2022-02-25 04:14:43 +0000

    p1 done

---
+++ p3original.c
@@ -0,0 +1,13 @@
+oint p1,p2;
+float distance;
+};
+struct _line Line;
+struct _polygon {
+int n;
+Line l[100];
+float perimenter;
+}
+int input_n();
+int input_polygon( int n, Polygon *p);
+Line input_line();
+void input_n_lines(int n, L
