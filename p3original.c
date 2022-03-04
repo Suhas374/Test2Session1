5cdc00106e140dccb360610843a92d9ec00e7170
suhas A <97019943+Suhas374@users.noreply.github.com>
2022-03-04 04:00:51 +0000

    p3 done

--- p3original.c
+++ p3original.c
@@ -1,13 +1,43 @@
-oint p1,p2;
-float distance;
-};
-struct _line Line;
-struct _polygon {
-int n;
-Line l[100];
-float perimenter;
+#include <stdio.h>
+int input_number()
+{
+  int n;
+  printf("enter the value of x\n");
+  scanf("%d",&n);
+  return n;
 }
-int input_n();
-int input_polygon( int n, Polygon *p);
-Line input_line();
-void input_n_lines(int n, L
+int is_prime(int n)
+{
+  for(int i=2;i<n;i++)
+    {
+      n=n%i;
+    }
+  if(n>0)
+  {
+    return 1;
+  }
+  else
+  {
+   return 0; 
+  }
+}
+void output(int n,int result)
+{
+  if(result>0)
+  {
+    printf("the number %d is a prime number\n",n);
+  }
+    else
+  {
+    printf("the number %d is not a prime number",n);
+  }
+}
+int main()
+{
+  int a;
+  a=input_number();
+  int result;
+  result=is_prime(a);
+  output(a,result);
+  return 0;
+  }
